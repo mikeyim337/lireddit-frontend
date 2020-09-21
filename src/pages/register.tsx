679a09c31792dc7f795795a01842f11bd438ff13
mikeyim337 <mk337337@gmail.com>
2020-09-20 19:37:11 -0700

    next js and chakra set up

---
+++ src/pages/register.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Formik, Form, setNestedObjectValues } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Box,
+  Button,
+} from "@chakra-ui/core";
+import { Wrapper } from "../components/wrapper";
+import { InputField } from "../components/InputField";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ values, handleChange, isSubmitting }) => (
+          <Form>
+            <Box mt={4}>
+              <InputField
+                name="username"
+                placeholder="username"
+                label="Username"
+              />
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              variantColor="teal"
+              isLoading={isSubmitting}
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
