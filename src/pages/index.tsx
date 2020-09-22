3434c277779870092f480e99755dd8a00efb3298
mikeyim337 <mk337337@gmail.com>
2020-09-22 15:37:10 -0700

    done with ssr setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
 import { NavBar } from "../components/NavBar";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "./../utils/createUrqlClient";
+import { usePostsQuery } from "./../generated/graphql";
+import { Divider } from "@chakra-ui/core";
 
-const Index = () => (
-  <>
-    <NavBar />
+const Index = () => {
+  const [{ data }] = usePostsQuery();
 
-    <div>Hello World</div>
-  </>
-);
+  return (
+    <>
+      <NavBar />
 
-export default Index;
+      <div>Hello World</div>
+
+      <br />
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </>
+  );
+};
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
